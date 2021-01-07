143e926701e6f5bdd7fdd196f946807b341389c7
Kuba <jciesmail@gmail.com>
2021-01-07 17:49:26 +0100

    Add dtov2

--- Data/FakturaRepo.cs
+++ Data/FakturaRepo.cs
@@ -20,7 +20,6 @@ namespace Data
     {
       var status = new List<FaktureStatus>
       {
-          new FaktureStatus{Id=0, Name="Nowa"},
           new FaktureStatus{Id=1, Name="DoZatwierdzenia"},
           new FaktureStatus{Id=2, Name="Zatwierdzona"},
           new FaktureStatus{Id=3, Name="Anulowana"}
@@ -33,7 +32,6 @@ namespace Data
     {
       var products = new List<FaktureProducts>
       {
-        new FaktureProducts{Id=0, Name="Narty", Count=2, Price=100, Tax=0.15f},
         new FaktureProducts{Id=1, Name="Deska", Count=5, Price=200, Tax=0.17f},
         new FaktureProducts{Id=2, Name="Czapka", Count=7, Price=50, Tax=0.2f},
         new FaktureProducts{Id=3, Name="Kurtka", Count=1, Price=70, Tax=0.19f},
@@ -47,12 +45,14 @@ namespace Data
     {
       var factures = new List<Fakture>
       {
-        new Fakture{Id=0, Code=20, Date=new DateTime(2020, 3, 4), City="Krakow", FaktureProductsId=2, FaktureStatusId=5},
         new Fakture{Id=1, Code=40, Date=new DateTime(2020, 4, 1), City="Warszawa", FaktureProductsId=1, FaktureStatusId=2},
-        new Fakture{Id=2, Code=90, Date=new DateTime(2020, 12, 6), City="Gdansk", FaktureProductsId=4, FaktureStatusId=1}
+        new Fakture{Id=2, Code=90, Date=new DateTime(2020, 12, 6), City="Gdansk", FaktureProductsId=4, FaktureStatusId=1},
+        new Fakture{Id=3, Code=90, Date=new DateTime(2020, 12, 6), City="Gdansk", FaktureProductsId=4, FaktureStatusId=1},
+        new Fakture{Id=4, Code=90, Date=new DateTime(2020, 12, 6), City="Gdansk", FaktureProductsId=4, FaktureStatusId=1},
+        new Fakture{Id=5, Code=90, Date=new DateTime(2020, 12, 6), City="Gdansk", FaktureProductsId=4, FaktureStatusId=1}
       };
 
-    
+
       var status = GetFactureStatus();
       var products = GetFactureProducts();
 
@@ -60,14 +60,14 @@ namespace Data
         x => x.FaktureStatusId,
         y => y.Id,
         (factures, status) => new FactureToReturnDto
-          {
-            Id = factures.Id,
-            Code = factures.Code,
-            Date = factures.Date,
-            City = factures.City,
-            Name = status.Name,
-            FaktureProductsId = factures.FaktureProductsId
-          }
+        {
+          Id = factures.Id,
+          Code = factures.Code,
+          Date = factures.Date,
+          City = factures.City,
+          Name = status.Name,
+          FaktureProductsId = factures.FaktureProductsId
+        }
       ).ToList();
 
       return facture;
